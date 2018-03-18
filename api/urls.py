c0a8f44367ffb484ae4cd3953fda12e628ba0f29
Tarun <tarun@ubuntu.ubuntu-domain>
2018-03-18 18:05:58 +0530

    added redirect

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    # url(r'^/', views.index, name='index'),
-    url(r'^fetch/short-url/', views.short_url, name='short-url'),
-
+    url(r'^fetch/short-url', views.short_url, name='short-url'),
+    url(r'^fetch/long-url', views.long_url, name='long-url'),
+    url(r'^fetch/short-urls', views.short_urls, name='short-urls'),
+    url(r'^fetch/long-urls', views.long_urls, name='long-urls'),
+    url(r'^fetch/count', views.count, name='count'),
+    url(r'^clean-urls', views.clean_urls, name='clean-urls'),
+    url(r'^(?P<unique_hash>.*)', views.url_redirect, name='url-redirect'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,16 +7,10 @@ from rest_framework.decorators import api_view
 from urlparse import urlparse
 from api import params
 import models
+from django.shortcuts import redirect
+from django.http import Http404
 
 # Create your views here.
-def index(request):
-    response = {
-        "status":"OK"
-    }
-
-    return JsonResponse(response)
-
-
 @api_view(['POST'])
 def short_url(request):
     response = {
@@ -24,7 +18,10 @@ def short_url(request):
         "status_codes": []
     }
     post_data = request.data
-
+    if "long_url" not in post_data:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["BAD_DATA"]
+        return JsonResponse(response)
     long_url = post_data.get('long_url','').rstrip('/')
     try:
         valid_url = bool(urlparse(long_url).scheme)
@@ -42,5 +39,165 @@ def short_url(request):
 
     short_url = params.BASE_URL+str(url.unique_hash)
     response['short_url'] = short_url
+    response['status'] = 'OK'
+    return JsonResponse(response)
+
+@api_view(['POST'])
+def long_url(request):
+    response = {
+        "status": "",
+        "status_codes": []
+    }
+    post_data = request.data
+    if "short_url" not in post_data:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["BAD_DATA"]
+        return JsonResponse(response)
+    short_url = post_data.get('short_url','').rstrip('/')
+    unique_hash = short_url.split(params.BASE_URL)[-1]
+
+    try:
+        url = models.URL.objects.get(unique_hash=unique_hash)
+    except:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["SHORT_URLS_NOT_FOUND"]
+        return JsonResponse(response)
+
+    long_url = url.long_url
+    response['long_url'] = long_url
+    response['status'] = 'OK'
+    return JsonResponse(response)
+
+@api_view(['POST'])
+def short_urls(request):
+    response = {
+        "status": "",
+        "status_codes": [],
+        "invalid_urls" : []
+    }
+    post_data = request.data
+    long_urls = post_data.get('long_urls',[])
+
+    if 'long_urls' not in post_data or type(long_urls) is not list:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["BAD_DATA"]
+        return JsonResponse(response)
+
+    invalid_urls = []
+    long_urls = [i.rstrip('/') for i in long_urls ]
+    valid_url = True
+    valid_long_urls = []
+    for long_url in long_urls:
+        try:
+            valid_url = bool(urlparse(long_url).scheme)
+        except:
+            valid_url = False
+        if valid_url:
+            valid_long_urls.append(long_url)
+        else:
+            invalid_urls.append(long_url)
+
+    urls = models.URL.objects.filter(long_url__in=valid_long_urls).values('long_url','unique_hash')
+    short_urls = {}
+    for url in urls:
+        short_urls[url['long_url']] = params.BASE_URL+str(url['unique_hash'])
+
+    found_urls = [o['long_url'] for o in urls]
+    new_urls = [i for i in valid_long_urls if i not in found_urls ]
+    for n in new_urls:
+        u = models.URL(
+            long_url = n
+        )
+        u.save()
+        short_urls[n] = params.BASE_URL+str(u.unique_hash)
+
+    response["short_urls"] = short_urls
+    if not valid_url:
+        response["invalid_urls"] = invalid_urls
+        response["status"] = "FAILED"
+        response["status_codes"] = ["INVALID_URLS"]
+    else:
+        response["status"] = "OK"
+    return JsonResponse(response)
+
+@api_view(['POST'])
+def long_urls(request):
+    response = {
+        "status": "",
+        "status_codes": [],
+        "invalid_urls" : []
+    }
+    post_data = request.data
+    short_urls = post_data.get('short_urls',[])
+
+    if 'short_urls' not in post_data or type(short_urls) is not list:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["BAD_DATA"]
+        return JsonResponse(response)
+
+    short_urls_dict = { s.rstrip('/').split(params.BASE_URL)[-1]:s for s in short_urls }
+    short_urls = [s.rstrip('/').split(params.BASE_URL)[-1] for s in short_urls]
+    urls = models.URL.objects.filter(unique_hash__in=short_urls).values('long_url', 'unique_hash')
+    long_urls = {}
+    for url in urls:
+        long_urls[params.BASE_URL+url['unique_hash']] = url['long_url']
+    response["long_urls"] = long_urls
+    found_urls = [o['unique_hash'] for o in urls]
+    new_urls = [short_urls_dict[s] for s in short_urls if s not in found_urls]
+
+    if new_urls:
+        response["invalid_urls"] = new_urls
+        response["status"] = "FAILED"
+        response["status_codes"] = ["INVALID_URLS"]
+    else:
+        response["status"] = "OK"
+    return JsonResponse(response)
+
+
+def url_redirect(request, unique_hash=""):
+    try:
+        url = models.URL.objects.get(unique_hash=unique_hash.rstrip('/'))
+        url.count = url.count + 1
+        url.save()
+        long_url = url.long_url
+    except:
+        raise Http404()
+    return redirect(long_url)
+
+@api_view(['POST'])
+def count(request):
+    response = {
+        "status": '',
+        "status_codes": []
+    }
+
+    post_data = request.data
+    if 'short_url' not in post_data:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["BAD_DATA"]
+        return JsonResponse(response)
+    short_url = post_data.get('short_url','').rstrip('/')
+    unique_hash = short_url.split(params.BASE_URL)[-1]
+    try:
+        url = models.URL.objects.get(unique_hash=unique_hash)
+    except:
+        response['status'] = 'FAILED'
+        response['status_codes'] = ["SHORT_URLS_NOT_FOUND"]
+        return JsonResponse(response)
+
+    response["count"] = url.count
+    response['status'] = 'OK'
+    return JsonResponse(response)
+
+def clean_urls(request):
+    response = {
+        "status": '',
+        "status_codes": []
+    }
+    try:
+        models.URL.objects.all().delete()
+    except:
+        response['status'] = 'FAILED'
+        return JsonResponse(response)
     response['status'] = 'OK'
     return JsonResponse(response)
